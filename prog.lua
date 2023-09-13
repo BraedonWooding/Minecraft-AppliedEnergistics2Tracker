a06e09690ad20574e781a11b9be0039b75ad1067
Braedon <Braedonww@gmail.com>
2023-09-13 19:53:59 +1000

    Change

--- prog.lua
+++ prog.lua
@@ -1,10 +1,25 @@
 local forms = require("forms")
+local component = require("component")
+
+meController = component.proxy(component.me_controller.address)
+
+require("track")
 
 local Form = forms.addForm()
 
 Form:setActive()
 
-Title = Form:addLabel(0, 0, "Item Tracking")
+local width = Form.W
 
-while true:
-    forms.run(Form)
+Title = Form:addLabel(width / 2 - 5, 1, "Item Tracking")
+
+for item in main do
+    name = item[1]
+
+    print(name)
+end
+
+Form:redraw()
+
+-- while true:
+--     forms.run(Form)
---
+++ track.lua
@@ -0,0 +1,11 @@
+main = {
+    { "Gold Ingot" },
+    { "Iron Ingot" },
+    { "Silver Ingot" },
+    { "Nickel Ingot" },
+    { "Ardite Ingot" },
+    { "Cobalt Ingot" },
+    { "Lead Ingot" },
+    { "Tin Ingot" },
+    { "Platinum Ingot" },
+}
